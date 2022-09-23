50eb145afd5452db1c186729b3540f45a4ccce54
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2022-09-23 14:19:09 +0200

    Linked mobile nav to sections

--- js/nav.js
+++ js/nav.js
@@ -13,3 +13,51 @@ navToggle.addEventListener("click", () => {
 	}
 
 });
+
+const sites = document.querySelector(".sites");
+
+
+sites.addEventListener("click", () => {
+	const visibility = sites.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		nav.setAttribute("data-visible", "true");
+		navToggle.setAttribute("aria-expanded", "true");
+	} else {
+		nav.setAttribute("data-visible", "false");
+		navToggle.setAttribute("aria-expanded", "false");
+	}
+});
+
+
+
+const about = document.querySelector(".about");
+
+
+about.addEventListener("click", () => {
+	const visibility = about.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		nav.setAttribute("data-visible", "true");
+		navToggle.setAttribute("aria-expanded", "true");
+	} else {
+		nav.setAttribute("data-visible", "false");
+		navToggle.setAttribute("aria-expanded", "false");
+	}
+});
+
+
+const contact = document.querySelector(".contact");
+
+
+contact.addEventListener("click", () => {
+	const visibility = contact.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		nav.setAttribute("data-visible", "true");
+		navToggle.setAttribute("aria-expanded", "true");
+	} else {
+		nav.setAttribute("data-visible", "false");
+		navToggle.setAttribute("aria-expanded", "false");
+	}
+});
